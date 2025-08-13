636518a8e001a272423c310a807e530d8ce96b25
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2025-08-13 10:48:22 +0300

    Modifed header

--- src/components/simpleheader.js
+++ src/components/simpleheader.js
@@ -2,43 +2,37 @@ import React from "react"
 
 const SimpleHeader = () => {
   return (
-    <header className="bg-black">
+    <header className="bg-black border-b border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-center items-center py-4">
-          
-          {/* Left Navigation - AirHUD */}
-          <nav className="font-manrope mr-8">
-            <a 
-              href="https://airhud.io"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white hover:text-brandorange font-medium transition-colors text-lg"
-            >
-              AirHUD
-            </a>
-          </nav>
-          
-          {/* Centered Logo */}
+        <div className="flex justify-between items-center py-4">
+          {/* Logo - Left */}
           <div className="flex-shrink-0">
             <img 
               src="https://energized-canvas-6b08f4eb08.media.strapiapp.com/anarky_Labs_Logo_White_794a742632.jpg"
               alt="Anarky Labs"
-              className="h-[200px] w-[170px] object-contain"
+              className="h-[120px] w-[102px] sm:h-[200px] sm:w-[170px] object-contain"
             />
           </div>
           
-          {/* Right Navigation - AirSkill */}
-          <nav className="font-manrope ml-8">
+          {/* Navigation - Right */}
+          <nav className="flex space-x-4 sm:space-x-8 font-manrope">
+            <a 
+              href="https://airhud.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-brandorange font-medium transition-colors text-sm sm:text-lg"
+            >
+              AirHUD
+            </a>
             <a 
               href="https://airskill.io"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-white hover:text-brandorange font-medium transition-colors text-lg"
+              className="text-white hover:text-brandorange font-medium transition-colors text-sm sm:text-lg"
             >
               AirSkill
             </a>
           </nav>
-          
         </div>
       </div>
     </header>
